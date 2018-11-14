6a5400ca6d641239d271802612ec2bb39a62b284
Sergiu Costache <costache.sergiu@gmail.com>
2018-11-14 02:50:56 +0200

    added collect fee script

---
+++ collect_fee.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cli="bzedge-cli"
+
+echo -e "\n======== z_listoperationids executing ============"
+$cli z_listoperationids executing
+
+echo -e "\n======== z_listoperationids queued ============"
+$cli z_listoperationids queued
+
+#$cli z_shieldcoinbase t1N3tJqRgxxH1cHtJayzXGQJw74dQ7ev4dq zcHJ8ZQfvkj6xs12rkTaSKN15bRXCghMohi16DQfkA1aMe6DqNXLDiCDQdRDeySAnS1pAu6wYxdcwMEZP5ckKRcxiFGUTrm 0.0001 243
+
+
+#$cli z_getbalance zcHJ8ZQfvkj6xs12rkTaSKN15bRXCghMohi16DQfkA1aMe6DqNXLDiCDQdRDeySAnS1pAu6wYxdcwMEZP5ckKRcxiFGUTrm 0
+
+#$cli z_sendmany zcHJ8ZQfvkj6xs12rkTaSKN15bRXCghMohi16DQfkA1aMe6DqNXLDiCDQdRDeySAnS1pAu6wYxdcwMEZP5ckKRcxiFGUTrm "[{\"address\":\"t1cyjhcVjz5UAv6yiyRCPDb3DPT9y1Up3Bv\", \"amount\":42574.9991}]"
+
+#$cli z_getoperationresult
+
+#$cli gettransaction
